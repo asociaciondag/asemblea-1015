aa7e3dfc09d3eb60008e7fcfab85e557a3f46ae0
Oscar Otero <oom@oscarotero.com>
2023-05-14 13:38:12 +0200

    pasados os arquivos

---
+++ _config.ts
@@ -0,0 +1,12 @@
+import lume from "lume/mod.ts";
+import postcss from "lume/plugins/postcss.ts";
+import inline from "lume/plugins/inline.ts";
+
+const site = lume();
+
+site.use(postcss());
+site.use(inline());
+site.copy("img");
+site.copy("fonts");
+
+export default site;
